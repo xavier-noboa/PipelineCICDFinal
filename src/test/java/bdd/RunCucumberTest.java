daf212677213deddff607d39e82dbae856a252c9
xavier-noboa <73750328+xavier-noboa@users.noreply.github.com>
2025-11-28 13:24:57 -0500

    Update RunCucumberTest.java

--- src/test/java/bdd/RunCucumberTest.java
+++ src/test/java/bdd/RunCucumberTest.java
@@ -4,11 +4,13 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "bdd")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
-    // Clase vacía: el runner de JUnit 5 + Cucumber se encarga de todo
+    // Runner sin cuerpo; JUnit 5 detecta y ejecuta los escenarios
 }
