af6697d891402df05ace241124b8ef8f2573e44a
xavier-noboa <73750328+xavier-noboa@users.noreply.github.com>
2025-11-28 17:54:40 -0500

    Create Paciente.java

---
+++ src/main/java/pacientes/Paciente.java
@@ -0,0 +1,13 @@
+package pacientes;
+
+public class Paciente {
+    private String nombre;
+
+    public Paciente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
