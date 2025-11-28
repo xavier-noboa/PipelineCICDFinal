46781ce292325f27af0802bcbfb4002671d038b5
xavier-noboa <73750328+xavier-noboa@users.noreply.github.com>
2025-11-28 18:09:49 -0500

    Create Medico.java

---
+++ src/main/java/medicos/Medico.java
@@ -0,0 +1,13 @@
+package medicos;
+
+public class Medico {
+    private String nombre;
+
+    public Medico(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
